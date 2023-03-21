e003c983e5d4abe38b466a18441e213420067413
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-03-21 21:01:31 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void) /* main - The Begaininng of the code */
+/** main - The Begaininng of the code 
+* then return
+*/
+
+int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
