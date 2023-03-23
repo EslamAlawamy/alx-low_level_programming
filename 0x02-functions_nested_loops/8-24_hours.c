cea9e4d16806d126cabf4f9e7825a8b4d0ad5f49
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-03-23 09:36:36 +0200

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,26 +7,17 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
-
-	for (a = 48; a <= 50; a++)
+	int h;
+	int m;
+	
+	for (h = 0; h < 25; h++)
 	{
-		for (b = 48; b <= 57; b++)
+		for (m = 0; m < 61 ; m++)
 		{
-			for (c = 48; c <= 53; c++)
-			{
-				for (d = 48; d <= 57; d++)
-				{
-					if (a >= 50 && b >= 52)
-						break;
-					_putchar(a);
-					_putchar(b);
-					_putchar(58);
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
-				}
-			}
+			_putchar(h);
+			_putchar(:);
+			_putchar(m);
+			
 		}
 	}
 }
