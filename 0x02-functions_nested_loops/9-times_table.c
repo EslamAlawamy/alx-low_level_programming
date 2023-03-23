3a1e03efecbc6f7d52113ec4517aef1d874d222b
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-03-23 10:52:01 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,21 +10,22 @@ void times_table(void)
 	int a;
 	int b;
 	int eq;
-	eq = a * b;
 		
 	for (a = 0; a < 10; a++)
 	{	_putchar('0');
 		
+	 
 		for (b = 1; b < 10; b++)
 		{
+		eq = a * b;	
 		_putchar(',');
 		_putchar(' ');
 		}
-	 if (eq < 10)
-	 {
+	 		if (eq < 10)
+	 	{
 		 _putchar(' ');
 		 _putchar(eq + '0');
-	 }
+	 	}
 		 
 		
 		
