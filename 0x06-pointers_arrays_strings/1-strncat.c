309c927fba504cd99c17d7c17c66572d764d415f
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-03-30 21:24:20 +0200

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,20 @@
+#include "main.h"
+/**
+ * _strncat - main
+ * @dest: hel
+ * @src: helo
+ * @n:hello
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+  int e;
+  int s;
 
+  for (e = 0; dust[e] != '\0'; e++)
+    ;
+  for (s = 0; src[s] != '\0'; s++)
+  {
+    dest[e + s] = src[s];
+  }
+  return (dest);
+}
