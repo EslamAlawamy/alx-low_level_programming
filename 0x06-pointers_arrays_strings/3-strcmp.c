d2f67f21b518e0c442d7c43604d2920cb872d1ed
Eslam Alawamy <eslamalawamy.com@gmail.com>
2023-03-30 14:09:06 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcmp - copies a string
+ * @s1: destination.
+ * @s2: source.
+ * Return: the pointer to dest.
+ */
+
+int _strcmp(char *s1, char *s2);
+{
+	int e;
+
+	for (e = 0; s1[e] && s2[e]; e++)
+	{
+		if (s1[e] != s2[e])
+			return (s1[e] - s2[e]);
+	}
+	return (s1[e] - s2[e]);
+}
