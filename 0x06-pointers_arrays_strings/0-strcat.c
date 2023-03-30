46f560f7f95192a77f6f2947c058d8326a52d7d2
Eslam Alawamy <eslamalawamy.com@gmail.com>
2023-03-30 11:55:07 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,16 +8,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
-        int e;
-        int s;
+	int e;
+	int s;
 
-        for (s = 0; dest[s] != '\0'; s++)
-                ;
-        for (e = 0; src[e] != '\0'; e++)
-        {
-                dest[s + e] = src[e];
-        }
+	for (s = 0; dest[s] != '\0'; s++)
+		;
+	for (e = 0; src[e] != '\0'; e++)
+	{
+		dest[s + e] = src[e];
+	}
 
-        dest[s + e] = '\0';
-        return (dest);
+	dest[s + e] = '\0';
+	return (dest);
 }
