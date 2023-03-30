716489e47cceb1dda475f792f3062321071e556c
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-03-30 23:18:39 +0200

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,13 @@ int _strcmp(char *s1, char *s2);
 {
 	int e;
 
-	for (e = 0; s1[e] == s2[e]; e++);
+	for (e = 0; s1[e] != '\0' && s2[e] != '\0' ; e++);
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[e] - s2[e]);
+		}
+	}
 		
 	return (s1[e] - s2[e]);
 }
