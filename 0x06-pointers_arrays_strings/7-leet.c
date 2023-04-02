4cba6a12d59e706ab3a315f2b5a083cd31ffcde6
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-04-02 23:33:57 +0200

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,23 +7,20 @@
 
 char *leet(char *s)
 {
-	int count = 0, i;
-	int low_letters[] = {97, 101, 111, 116, 108};
-	int upp_letters[] = {65, 69, 79, 84, 76};
-	int numbers[] = {52, 51, 48, 55, 49};
+	int i
+	int j;;
+	char o[11] = "aAeEoOtTlL";
+	char c[11] = "4433007711";
 
-	while (*(s + count) != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; i < 5; i++)
+		for (j = 0; j < 11; j++)
 		{
-			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+			if (s[i] == o[j])
 			{
-				*(s + count) = numbers[i];
-				break;
+				s[i] == c[j];
 			}
 		}
-		count++;
 	}
-
 	return (s);
 }
