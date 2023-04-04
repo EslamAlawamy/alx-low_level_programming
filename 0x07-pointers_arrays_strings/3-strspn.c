6154896ddb8d5d5144a9f3e8f3a1cb3721bb10e0
Eslam Alawamy <eslamalawamy.com@gmail.com>
2023-04-03 17:07:19 -0700

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strspn - main
+ * @s: firet
+ * @accept: second
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int e = 0, w, q = 0;
+
+	while (accept[e])
+	{
+		w = 0;
+
+		while (s[w] != 32)
+		{
+			if (accept[e] == s[w])
+			{
+				q++;
+			}
+			w++;
+		}
+			e++;
+	}
+	return (q);
+
+}
