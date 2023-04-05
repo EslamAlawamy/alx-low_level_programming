c7566410db30f0ea7faaeac3012e2a8160cb4d55
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-04-06 01:43:25 +0200

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _strlen_recursion - main
  * @s: var
- *
+ * Return: the final
  */
 int _strlen_recursion(char *s)
 {
@@ -11,5 +11,5 @@ int _strlen_recursion(char *s)
 		return (0);
 	}
 	else
-		return (1 + _strlen_recursion (s + 1));
+		return (1 + _strlen_recursion(s + 1));
 }
