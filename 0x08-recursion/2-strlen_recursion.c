6bf64be5b2993a57fa098e1bd1e6b4fd1c05c10c
Eslam Alawamy <eslamalawamy.com@gmail.com>
2023-04-05 16:39:33 -0700

    3

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - main
+ * @s: var
+ *
+ */
+int _strlen_recursion(char *s)
+{
+	int l;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+		return (1 + (_strlen_recursion + 1));
+}
