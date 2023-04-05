1e5c040c82bf9d31fb7da9efa33121c3c8a34de8
Eslam Alawamy <eslamalawamy.com@gmail.com>
2023-04-05 16:51:04 -0700

    4

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - main
+ * @n: var
+ * Return: the last
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+		return (1);
+	else
+		return (factorial = n * factorial(n - 1));
+}
