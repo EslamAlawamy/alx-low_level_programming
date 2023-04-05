efc69f7cdc2c0581505de7ff87e99ad9573b7978
Eslam Alawamy <eslamalawamy.com@gmail.com>
2023-04-05 16:20:23 -0700

    2

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *
- *
+ * _puts_recursion - main
+ * @s: var
  *
  */
 void _puts_recursion(char *s)
@@ -9,7 +9,7 @@ void _puts_recursion(char *s)
 	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(*s + 1)
+		_puts_recursion(s + 1)
 	}
 	else
 		_putchar('\n');
