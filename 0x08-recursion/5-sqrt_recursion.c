16dbf4bf59ccd1ae830c75de7e2ab353828c801b
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-04-06 02:57:01 +0200

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,34 +1,33 @@
 #include "main.h"
 
+
 /**
- * _sqrt_recursion - returns the natural square root of a number
- *
- * @n: number to calculate the square root of
- *
- * Return: the resulting square root
- */
+* Description: sqt - calculate the sqare root of string
+* @n: the number
+* @i: counter form [1 , n]
+* Return: sqt of n
+*/
+
+int sqt(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+
+	return (sqt(n, i + 1));
+}
+
+
+
+
+/**
+* Description: _sqrt_recursion - Return squre root
+* @n: int
+* Return: Integer
+*/
+
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	return (sqt(n, 1));
 }
-
-/**
- * actual_sqrt_recursion - recurses to find the natural
- * square root of a number
- *
- * @n: number to calculate the sqaure root of
- * @flag: flag number i will use to find sqaure root
- *
- * Return: flag or -1
- */
-int actual_sqrt_recursion(int n, int flag)
-{
-	if (flag * flag > n)
-		return (-1);
-	if (flag * flag == n)
-		return (flag);
-	return (actual_sqrt_recursion(n, flag + 1));
-}
-
