d1fd6cfa221b7e5d21d3d03080284b4733cfd779
Eslam Alawamy <eslamalawamy.com@gmail.com>
2023-04-09 12:49:06 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the main fun
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: is 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
