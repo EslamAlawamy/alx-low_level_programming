3dd90fb21f1fbf188ad1263866723bfe19e62d6d
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-04-10 23:23:42 +0200

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,27 +9,18 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j;
-	int sum = 0;
-	char c;
+	int i;
+	int sum;
 
-	if (argc == 1)
+	for (i = 0; i < argc; i++)
 	{
-		printf("0\n");
-		return (0);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (i < 48 || i > 57)
 		{
-			c = argv[i][j];
-			if (c < '0' || c > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
+			printf("Error\n");
+			return (1);
 		}
-		sum += atoi(argv[i]);
+		else
+			sum = sum + atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
