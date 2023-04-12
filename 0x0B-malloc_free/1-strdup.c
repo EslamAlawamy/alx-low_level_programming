27997fb8569b32b5de5a36603d75c3f81f31d0f7
Eslam Alawamy <eslamalawamy.com@gmail.com>
2023-04-12 06:33:52 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *_strdup(char *str)
+{
+	char *d;
+	int t;
+	int c;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (t = 0; str[t] != '\0'; t++ )
+		;
+	d = malloc(sizeof(char) * t );
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (c = 0; c < t; c++)
+		d[i] = str[c]	
+	}
+	return (d);
+}
