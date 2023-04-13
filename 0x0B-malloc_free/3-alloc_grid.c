6d121035bcd171dc881b6b594a9a460f7faac1cb
Eslam Alawamy <eslamalawamy.com@gmail.com>
2023-04-12 17:01:14 -0700

    201

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int w;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	gird = maloc(sizeof(int *) * height);
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (w = 0; w <= height; w++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+			if (grid[i] == NULL)
+			{
+				return (NULL);
+			}
+	}
+	for (w = 0; w < height; i++)
+	{
+		for (int j = 0;j <= height; j++)
+			grid[w][j] = 0;
+	}
+	return (grid);
+}
