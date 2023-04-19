f49cb33b3693b176fc4da9caf19c4041189861a4
Eslam Alawamy <eslamalawamy.com@gmail.com>
2023-04-19 14:45:17 -0700

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - main int_index
+ * @array: 1var
+ * @cmp: the fun
+ * @size: the size
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		if (size <= 0)
+		{
+			return (-1);
+		}
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
