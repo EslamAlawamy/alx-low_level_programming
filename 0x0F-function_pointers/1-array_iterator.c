7523380bc0dbd87f3bbf3250fa65ee6a2de9ab52
Eslam Alawamy <eslamalawamy.com@gmail.com>
2023-04-19 10:46:06 -0700

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,15 @@
 #include "function_pointers.h"
-
+/**
+ * array_iterator - main
+ * @array: arr
+ * @size: sizet
+ * @action: fun
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
 	if (array && size && action)
 		for (i = 0; i < size; i++)
-			action (array[i]);
+			action(array[i]);
 }
