3ccfe344d888cee038c96c4e48fbf9c99dd39b96
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-04-20 03:07:18 +0200

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -28,6 +28,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", operation(atoi(argv[1]), (atoi(argv[3])));
+	printf("%d\n", operation(atoi(argv[1]), (atoi(argv[3]))));
 	return (0);
 }
