8cbbe75ebd1517c827592a3cf0887944499b4df7
Eslam Alawamy <eslamalawamy.com@gmail.com>
2023-04-19 17:39:07 -0700

    3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - the main function
+ * @argc: the argcount
+ * @argv : the arg vector
+ * Return: success
+ */
+int main(int argc, char *argv[])
+{
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	operation = get_op_func(argv[2]);
+
+	if (operation == 0)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (((argv[2][0] == '/') || (argv[2][0] == '%')) && (argv[3] == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", operation(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "3-calc.h"
+/**
+ * op_add - addition fun
+ * @a: 1st num
+ * @b: 2sec num
+ * Return: add
+ */
+
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - subtraction fun
+ * @a: 1st num
+ * @b: 2sec num
+ * Return: subtraction
+ */
+
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - multiplication fun
+ * @a: 1st num
+ * @b: 2sec num
+ * Return: multiplication
+ */
+
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - division fun
+ * @a: 1st num
+ * @b: 2sec num
+ * Return: division
+ */
+
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - modulo fun
+ * @a: 1st num
+ * @b: 2sec num
+ * Return: modulo
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
