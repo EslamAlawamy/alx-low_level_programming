a2b8b35b7a2e6a4e2d2d0f24fc55f15494e41c2d
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-04-28 03:28:13 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,9 +14,10 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 	str_new = strdup(str);
-	if (str_new == NULL)
-		return (NULL);
+
+	str_len = strlen(str_new)
 	new_node->str = str_new;
+	new_node->len = str_len
 	new_node->next = (*head);
 	*head = new_node;
 	return (new_node);
