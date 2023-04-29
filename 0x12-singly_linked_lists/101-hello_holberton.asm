6cf6b4ea40017e5b5402fb8f491898de5c48c47b
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-04-28 17:38:11 -0700

    assem

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,22 @@
+    section .data
+message: db   "Hello, Holberton",0xA
+
+    global main
+    extern printf
+
+    section .text
+main:
+    ; function setup
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 32
+    ;
+    lea     rdi, [rel message]
+    mov     al, 0
+    call    printf
+
+    ; function return
+    mov     eax, 0
+    add     rsp, 32
+    pop     rbp
+    ret
