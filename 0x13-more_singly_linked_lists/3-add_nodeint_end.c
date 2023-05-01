59e01f38519a0b2e3c57a8fc71ff28401f909617
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-05-01 15:54:47 -0700

    3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new_node;
 	listint_t *last_node;
 
-        new_node = malloc(sizeof(head));
+	new_node = malloc(sizeof(head));
 	if (new_node == NULL)
 	{
 		return (NULL);
