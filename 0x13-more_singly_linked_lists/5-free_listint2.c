f743bf6e082492bfd3002f33dd87c8237ecdf834
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-05-01 18:02:56 -0700

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - free the list
+ * @head: the list element
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *current;
+
+	current = *head;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+	*head = NULL;
+}
