78651106ed5def09ffd33b13c8d91733436b2114
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-05-02 22:58:19 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 	temp = *head;
-	while (temp != 0 && i < idx)
+	while (i < idx)
 	{
 		temp = temp->next;
 		i++;
