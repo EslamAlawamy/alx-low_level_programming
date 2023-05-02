b1968f4b0e665ffbca018166942b9dc33739b084
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-05-02 04:04:00 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,10 @@ void free_listint2(listint_t **head)
 	listint_t *current;
 
 	current = *head;
-
+	if (head == NULL)
+        {
+                return;
+        }
 	while (current != NULL)
 	{
 		temp = current;
