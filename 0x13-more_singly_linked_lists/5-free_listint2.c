db1e16f8b82abba3cd1e4cc73a31dc3e76fafe4d
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-05-01 18:05:39 -0700

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,9 +10,9 @@ void free_listint2(listint_t **head)
 
 	current = *head;
 	if (head == NULL)
-        {
-                return;
-        }
+	{
+		return;
+	}
 	while (current != NULL)
 	{
 		temp = current;
