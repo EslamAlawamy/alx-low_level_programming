5f14fde2c9f3f73bc8fcafae5a25e9a7db9eaf77
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-05-04 15:27:34 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_binart - print binary
+ * @n th ebinary var
+ * Return: the binary num
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}	
+}
