b25d879e970658d4905723ec0b9cd749b94771a6
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-05-08 16:58:05 -0700

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content  == NULL)
+	{
+		text_content = "";
+	}
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+	write(fd, text_content, i);
+	close(fd);
+	return (1);
+}
