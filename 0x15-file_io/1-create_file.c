66a4519dccd6912600ff30a0229f720adb160979
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-05-08 17:02:13 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * create_file - the main func
+ * @filename: the file name
+ * @text_content: the content
+ * Return: 1 if succsess
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
@@ -9,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
