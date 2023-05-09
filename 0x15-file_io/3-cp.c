e076b6ad8c3eadb30f4794babdc540471dbe47c3
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-05-09 05:06:21 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,24 +5,24 @@
  * @argv: arg vector
  * Return: is 0
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
 	int fd_to_write, fd_to_read;
 	int i = 0, buffer_read = BUFFER_SIZE;
 	char buf[BUFFER_SIZE];
 
-	if (ac != 3)
+	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	fd_to_read = open(av[1], O_RDONLY);
+	fd_to_read = open(argv[1], O_RDONLY);
 	if (fd_to_read == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
-	fd_to_write = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+	fd_to_write = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
 														  | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd_to_write == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_to_write), exit(99);
 	}
 
@@ -31,10 +31,10 @@ int main(int ac, char **av)
 	{
 		buffer_read = read(fd_to_read, buf, BUFFER_SIZE);
 		if (buffer_read == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 		i = write(fd_to_write, buf, buffer_read);
 		if (i < buffer_read)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (close(fd_to_read) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to_read), exit(100);
