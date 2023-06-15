f796838447fe2f28837caa9486b90d3b521fbd29
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-06-15 14:02:59 -0700

    5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - the main func
+ * @head: the header
+ * @index: the index of the element
+ * Return: nth node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+
+
+	while (head != 0)
+	{
+		if (index == 0)
+		{
+			return (head);
+		}
+		index++;
+		head = head->next;
+	}
+	return (NULL);
+}
