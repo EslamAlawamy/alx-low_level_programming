5f1574f6f8be9a1dbd11d0a8decfe1a93dfdfa9b
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-06-15 23:34:32 +0300

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -23,7 +23,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		last = *head;
 		while (last->next != NULL)
-			last =last->next;
+			last = last->next;
 		new_node->prev = last;
 		last->next = new_node;
 	}
